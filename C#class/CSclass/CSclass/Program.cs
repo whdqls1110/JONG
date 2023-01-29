bc13609ae8044cad5e3c08214f98c435fbbb3c09
Jongbeen <jongbeen@users.noreply.github.com>
2023-01-29 17:23:50 +0900

    2023/01/29

---
+++ C#class/CSclass/CSclass/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Console.Write("Hellow World");
+            //System.Console.WriteLine("내이름은 윤종빈 입니다.");
+            //System.Console.Write("나이는 29입니다.");
+            //System.Console.WriteLine("나는 백수 입니다.");
+            //System.Console.Write("인천 서구 청라동에 살고 있습니다.");
+            //System.Console.WriteLine("게임을 좋아합니다.");
+            //System.Console.Write("던파를 주로 합니다.");
+            //System.Console.WriteLine("주말엔 레이드를 니다.");
+            //System.Console.Write("오늘은 저녘에 레이드를 갑니다.");
+            //System.Console.WriteLine("11");
+            //System.Console.WriteLine("--------------------------------------------");
+            //System.Console.Write("1");
+            //System.Console.WriteLine("2");//WriteLine사용법
+            //System.Console.Write("3");//Write사용법
+            //System.Console.WriteLine("4");
+            //System.Console.Write("5");
+            //System.Console.WriteLine("6");
+            //System.Console.Write("7");
+            //System.Console.WriteLine("8");
+            //System.Console.Write("9");
+            //System.Console.Write("10");//타이핑 힘들어
+            //System.Console.WriteLine("------------------------------------------------");
+            ////데이터 형-String
+            //String myName;
+            //myName = "JongBeen";
+            //System.Console.WriteLine(myName);
+            //String myage;
+            //myage = "29";
+            //String myjob;
+            //myjob = "백수";
+            //String myAdress;
+            //myAdress = "인천 서구 청라동";
+            //String myToday;
+            //myToday = "레이드";
+            //System.Console.WriteLine(myage);
+            //System.Console.WriteLine(myjob);
+            //System.Console.WriteLine(myAdress);
+            //System.Console.WriteLine(myToday);
+            ////-----------------------------------------------
+            ////ReadLine();
+            //string Myname;
+            //string Myage;
+            //string Myadress;
+            //string ForM;
+            //System.Console.WriteLine("이름을 입력해 주세요.");
+            //Myname = System.Console.ReadLine();
+            //System.Console.WriteLine("나이를 입력해 주세요.");
+            //Myage = System.Console.ReadLine();
+            //System.Console.WriteLine("주소를 입력해주세요.");
+            //Myadress = System.Console.ReadLine();
+            //System.Console.WriteLine("성별을 입력해주세요.");
+            //ForM = System.Console.ReadLine();
+
+            //System.Console.Write("당신의 이름은 : ");
+            //System.Console.WriteLine(Myname);
+            //System.Console.Write("당신의 나이는 : ");
+            //System.Console.WriteLine(Myage);
+            //System.Console.Write("당신의 주소는 : ");
+            //System.Console.WriteLine(Myadress);
+            //System.Console.Write("당신의 성별은 : ");
+            //System.Console.WriteLine(ForM);
+
+            //System.Console.WriteLine($"당신의 이름은 {myName} 이고, 당신의 나이는 {Myage} 입니다.");
+            //System.Console.WriteLine("당신의 이름은 {0} 이고, 당신의 나이는 {1} 입니다.", Myname, Myage);
+            ////---------------------------------------------------------------------------------
+            //System.Console.WriteLine(int.MaxValue);
+            //int i = 0;
+            //string ss = "22";
+            //float ff = 2.22f;
+            //string myName = "jongbeen";
+            //System.Console.Write(myName);
+            //string myAge = "29";
+            //string MMM = myName + myAge;
+            //System.Console.Write(MMM);
+            //string kk = null;//대상이 없다.
+            //System.Console.WriteLine("나이를 입력해주세요.");
+            //string myAge;
+            //myAge = System.Console.ReadLine();
+            //int myAgeNumber = Convert.ToInt32(myAge);
+            //string nowAge = (myAgeNumber + 3).ToString();
+
+            //나의 태어난 연도 입력하기
+            //받은 입력 후 지금 나이 도출하기
+            //string myBoon;
+            //System.Console.WriteLine("태어난 년도를 입력해 주세요.");
+            //myBoon = System.Console.ReadLine();
+            //int myBoonNumber = Convert.ToInt32(myBoon);
+            //string nowAge = (2023 - myBoonNumber).ToString();
+            //System.Console.Write(nowAge);
+            //            System.Console.WriteLine(@"abc
+            //defg
+            //gdds
+            //hdef
+            //sdasf
+            //sffd");
+            //            System.Console.WriteLine(@"     Q
+            //    QQQ
+            //   QQQQQ
+            //  QQQQQQQ
+            // QQQQQQQQQ
+            //QQQQQQQQQQQ");
+            //            //1.변수형 종류 주석으로 작성 후,각 변수형들의 최댓값 최솟값을 출력하기
+            //            //int
+            //            System.Console.WriteLine("Max int : " + int.MaxValue);
+            //            System.Console.WriteLine("Min int : " + int.MinValue);
+            //            //double
+            //            System.Console.WriteLine("Max double : " + double.MinValue);
+            //            System.Console.WriteLine("Min double : " + double.MinValue);
+            //            //float
+            //            System.Console.WriteLine("Max float : " + float.MaxValue);
+            //            System.Console.WriteLine("Min float : " + float.MinValue);
+            //            //long
+            //            System.Console.WriteLine("Max long : " + long.MaxValue);
+            //            System.Console.WriteLine("Min long : " + long.MinValue);
+
+            //            //2.string으로 이름 나이 입력받고 ,출력하기
+            //            string myName;
+            //            string myAge;
+            //            System.Console.WriteLine("이름을 입력해 주세요.");
+            //            myName = System.Console.ReadLine();
+            //            System.Console.WriteLine("나이를 입력해 주세요.");
+            //            myAge = System.Console.ReadLine();
+            //            System.Console.WriteLine("당신의 이름은" + myName + "입니다.");
+            //            System.Console.WriteLine("당신의 나이는" + myAge + "입니다.");
+
+            //            //3.@사용하여 별 그리기
+            //            System.Console.WriteLine(@"aaaaaaaaaaaaaaaa  aaaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaaa    aaaaaaaaaaaaaaaa
+            //aaaaaaaaaaaaaa      aaaaaaaaaaaaaaa
+            //aaaaaaaaaaaa          aaaaaaaaaaaaa
+            //aa                               aa
+            //aaaaa                         aaaaa
+            //aaaaaaa                     aaaaaaa
+            //aaaaaaaaa                 aaaaaaaaa
+            //aaaaaaa         aaaa      aaaaaaaaa
+            //aaaaaaa     aaaaaaaaaaa     aaaaaaa
+            //aaaaaa   aaaaaaaaaaaaaaaaa   aaaaaa
+            //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+            //            //4.int값을 float로 캐스팅하기
+            //            System.Console.WriteLine("숫자를 입력하세요.");
+            //            string ss = System.Console.ReadLine();
+            //            int i = Convert.ToInt32(ss);
+            //            float f = Convert.ToSingle(i);
+            //            string nowfloat = Convert.ToString(f);
+            //            System.Console.WriteLine(nowfloat);
+
+            //            //3.12를 int값으로 캐스팅하기
+            //            float ff = 3.12f;
+            //            int ii = Convert.ToInt32(ff);
+            //            string nowii = Convert.ToString(ii);
+            //            System.Console.WriteLine(nowii);
+
+            //            //이름 입력받고
+            //            //입력받은 이름의 길이를 숫자로 바꾸고
+            //            //받은 숫자를 출력한다.
+            //            System.Console.WriteLine("이름을 입력해 주세요.");
+            //            string MyName = System.Console.ReadLine();
+            //            int lnMyName = MyName.Length;
+            //            string nowLn = Convert.ToString(lnMyName);
+            //            System.Console.WriteLine(nowLn);
+
+            //값을 가져올 때 형식을 모를 때
+            //var MyName = "윤종빈";
+            //var iii = 3;
+            //var ffff = 3.12f;
+            int[] iii = new int[3];
+
+            int[] kkk = new int[4] { 1, 2, 3, 4 };
+
+            string[] lang = new string[3] { "c#", "c++", "c" };
+            long[] lolo = new long[1] { 2 };
+
+            System.Console.WriteLine(kkk[0]);
+
+            //int형 배열 5칸짜리 5개
+            //string형 배열 5칸짜리 5개
+
+            int[] inin = new int[5] { 8, 3, 6, 4, 56 };
+            string[] str = new string[5] { "str", "int", "long", "bool", "float" };
+            System.Console.WriteLine(inin[3]);
+            System.Console.WriteLine(str[2]);
+
+            int[] LaterArray;
+            System.Console.WriteLine("배열의 크기는?");
+            string ArraySize;
+            ArraySize = System.Console.ReadLine();
+            LaterArray = new int[Convert.ToInt32(ArraySize)];
+
+            string[] llang = new string[3];
+            llang[0] = "c";
+            llang[1] = "c++";
+            llang[2] = "c#";
+            System.Console.WriteLine($"0번 값은 {llang[0]}입니다.");
+            llang[0] = "나도졸려";
+            System.Console.WriteLine($"0번 값은 {llang[0]}입니다.");
+        }
+    }
+}
