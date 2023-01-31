b0f244286f5fc6b76e223490362d6bd16f6aba33
Jongbeen <jongbeen@users.noreply.github.com>
2023-01-31 17:52:43 +0900

    첫주차 숙제

---
+++ Homework2023.1st/Homework2023.1st/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2023._1st
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("제 이름은 윤종빈 입니다.");
+            System.Console.WriteLine("제 나이는 29살 입니다.");
+            System.Console.WriteLine("저는 수학과 과학을 좋아합니다.");
+            System.Console.WriteLine("저는 2년전까지 수재품 공방에서 1년정도 일하다가 그만두고 현제는 무직 입니다.");
+            System.Console.Write("여기 있는 사람들이 대체로 그럴꺼라 생각하지만");
+            System.Console.WriteLine(" 제 취미는 게임입니다.");
+            System.Console.WriteLine("주로 하는 게임은 던전엔 파이터 입니다.");
+            System.Console.WriteLine("최근에는 스팀에서 스컬도 즐겨하고 있습니다.");
+            System.Console.WriteLine("주말에는 주로 레이드를 하며 시간을 보넵니다.");
+            System.Console.WriteLine("저는 유니티 기초반 수업을 이수 했습니다.");
+            System.Console.WriteLine("처음에는 일인 게발자가 되려고 했지만 지금은 절대로 하기 싫습니다.");
+        }
+    }
+}
